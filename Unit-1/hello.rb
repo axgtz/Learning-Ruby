1ca5434df88b8bf120615e71c3d057e7324005f7
axgtz <axguti@gmail.com>
2017-04-05 23:29:32 -0500

    First commit

---
+++ Unit-1/hello.rb
@@ -0,0 +1,10 @@
+#Todas las funciones en ruby son objetos#
+#Para correrlo en la terminal, ir al directorio, correr el ruby interpreter: ruby hello.rb#
+
+#Imprimir en la pantalla#
+puts "Favor de escribir su nombre abajo"
+
+
+#Input de user#
+name = gets
+puts "Hello, " + name
