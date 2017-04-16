362ab8fb768405b52ccbcdcbdaf69a05fb6874a1
axgtz <axguti@gmail.com>
2017-04-15 23:42:23 -0500

    Added comment, perimeter override not working

--- Basics/inheritance.rb
+++ Basics/inheritance.rb
@@ -16,7 +16,29 @@ class Rectangle
   end
 end
 
+=begin
 r = Rectangle.new(2,4)
 
 puts "Area = #{r.area}, Perimenter = #{r.perimeter}"
 puts "#{r.to_s}"
+=end
+
+#"is a " = rectangle, square is a retangle
+
+#Square inherits from rectangle
+class Square < Rectangle #Square recieves the rectangle as its parent
+  def initialize(side)
+    super(side, side)#Llamando a super, llamas al constructor de la clase padre
+  end
+
+  #Overrides the definition of perimiter of the father
+  def perimeter
+    4*@w
+  end
+end
+
+sq = Square.new(3)
+
+puts "#{sq.perimeter}"
+
+#not working
