6f66ba907ce49e1c47ffad6ff448c448f604a1b9
axgtz <axguti@gmail.com>
2017-04-21 20:40:47 -0500

    added comments

--- Basics/accessPrivProtectPublic.rb
+++ Basics/accessPrivProtectPublic.rb
@@ -1,5 +1,13 @@
+=begin
+  Acceso privado no es lo mismo que en c++ y en java, es casi igual que protected pero con una diferencia
+=end
 class Test
-  def initialize(a) @a =a end
+  #Initialize method is protected, no se puede acceder fuera de la clase o de cualquiera de sus subclases
+  def initialize(a)
+    #Attributes in clases are default protected
+    @a =a
+  end
+  #Methods defined in a class are always public
 
   #Reciever is implicit
   def getA_implicit() @a end
