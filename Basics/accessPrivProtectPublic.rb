736c8890cdbb74c19bc04bb56b152d8524e42fb8
axgtz <axguti@gmail.com>
2017-05-15 23:41:26 -0500

    commit

--- Basics/accessPrivProtectPublic.rb
+++ Basics/accessPrivProtectPublic.rb
@@ -1,6 +1,6 @@
 =begin
   Acceso privado no es lo mismo que en c++ y en java, es casi igual que protected pero con una diferencia,
-  los metodos privados no pueden ser llamados por un 'explicit reciever' video 1:57
+  los metodos privados no pueden ser llamados por un 'explicit reciever' donde me quede video 1:57
 =end
 class Test
   #Initialize method is protected, no se puede acceder fuera de la clase o de cualquiera de sus subclases
